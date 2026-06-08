b3034122d4865785fe3e5e43b1b964b8bd20a5bd
Matihercas11 <matihercas11@gmail.com>
2026-06-08 10:30:49 -0600

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    host: "0.0.0.0",
+    port: 80,
+    allowedHosts: [
+      "web-cuwc67h0tfqi.up-de-fra1-k8s-1.apps.run-on-seenode.com"
+    ]
+  }
+});
